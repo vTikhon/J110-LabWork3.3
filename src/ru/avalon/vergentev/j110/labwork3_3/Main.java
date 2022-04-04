7a79e34da2f2d71c37f4c2b6e0e7701d0a59823d
vTikhon <tikhon.v@gmail.com>
2022-04-04 11:12:17 +0300

    непонятно должен ли переопределяться массив при реализации добавления нового элемента списка в его начало. например добавляем в список A,B,C,D,E,F
    вариант1:
    F, null.null,null,null
    A,B,C,D,E
    
    вариант2:
    F,A,B,C,D
    E,null,null,null,null

--- src/ru/avalon/vergentev/j110/labwork3_3/Main.java
+++ src/ru/avalon/vergentev/j110/labwork3_3/Main.java
@@ -3,19 +3,19 @@ package ru.avalon.vergentev.j110.labwork3_3;
 public class Main {
     public static void main(String[] args) {
         List myList1 = new List();
-        myList1.addToEnd("GGG");
-        myList1.addToEnd("HHH");
-        myList1.addToEnd("III");
-        myList1.addToEnd("JJJ");
-        myList1.addToEnd("KKK");
-        myList1.addToEnd("LLL");
-        myList1.addToEnd("MMM");
+//        myList1.addToEnd("HHH");
+//        myList1.addToEnd("III");
+//        myList1.addToEnd("JJJ");
+//        myList1.addToEnd("KKK");
+//        myList1.addToEnd("LLL");
+//        myList1.addToEnd("MMM");
         myList1.addToBegin("AAA");
-//        myList1.addToBegin("BBB");
-//        myList1.addToBegin("CCC");
-//        myList1.addToBegin("DDD");
-//        myList1.addToBegin("EEE");
-//        myList1.addToBegin("FFF");
+        myList1.addToBegin("BBB");
+        myList1.addToBegin("CCC");
+        myList1.addToBegin("DDD");
+        myList1.addToBegin("EEE");
+        myList1.addToBegin("FFF");
+//        myList1.addToBegin("GGG");
         myList1.print(); //выводим сформированный список
 //        myList1.modifyElement("zzz"); //Добавляем к элементам списка GGG
 //        myList1.print(); //выводим модифицированный список
