463cfd5934bf6649fcd1a6516064bcc7513e451a
vTikhon <tikhon.v@gmail.com>
2022-04-04 10:28:25 +0300

    нашёл ещё одну несостыковку с записью при чередовании записи в конец и начало списка

--- src/ru/avalon/vergentev/j110/labwork3_3/List.java
+++ src/ru/avalon/vergentev/j110/labwork3_3/List.java
@@ -24,19 +24,23 @@ public class List {
             element.array[i] = data;
             head = element;
             tail = element;
+            System.out.println("begin 111 " + element.array[i]);
             i++;
         } else if (i==0) {
             Linker element = new Linker(new String[5]);
             element.next = head;
             head = element;
             element.array[i] = data;
+            System.out.println("begin 222 " + element.array[i]);
             i++;
         } else {
             Linker element = head;
             element.array[i] = data;
             if (element.array[element.array.length - 1] == null) {
+                System.out.println("begin 333 " + element.array[i]);
                 i++;
             } else {
+                System.out.println("begin 444 " + element.array[i]);
                 i = 0;
             }
         }
@@ -48,33 +52,37 @@ public class List {
             element.array[i] = data;
             head = element;
             tail = element;
+            System.out.println("end 111 " + element.array[i]);
             i++;
         } else if (i==0) {
             Linker element = new Linker(new String[5]);
             tail.next = element;
             tail = element;
             element.array[i] = data;
+            System.out.println("end 222 " + element.array[i]);
             i++;
         } else {
             Linker element = tail;
             element.array[i] = data;
             if (element.array[element.array.length - 1] == null) {
+                System.out.println("end 333 " + element.array[i]);
                 i++;
             } else {
+                System.out.println("end 444 " + element.array[i]);
                 i = 0;
             }
         }
     }
 
-    //метод печатания заданного в main списка с проверкой его пустоты
+    //метод печатания заданного списка с проверкой его пустоты
     public void print () {
         Linker element = head;
         if (element != null) {
             while (element != null) {
-                for (int j = 0; j < element.array.length; j++) {
-                    System.out.print(element.array[j] + ", ");
+                for (int i = 0; i < element.array.length-1; i++) {
+                    System.out.print(element.array[i] + ", ");
                 }
-                System.out.println('\n');
+                System.out.println(element.array[element.array.length-1]);
                 element = element.next;
             }
             System.out.print('\n');
@@ -83,76 +91,126 @@ public class List {
         }
     }
 
-//    //метод извлечения элемента из начала списка
-//    public void extractionFromBegin () {
-//        Linker element = head;
-//        System.out.println(element.data);
-//    }
-//    //метод извлечения элемента из конца списка
-//    public void extractionFromEnd () {
-//        Linker element = tail;
-//        System.out.println(element.data);
-//    }
-//
-//    //метод удаления элемента из начала списка
-//    public void removingFromBegin () {
-//        head = head.next;
-//    }
-//    //метод удаления элемента из конца списка
-//    public void removingFromEnd () {
-//        Linker element = head;
-//        while (element != tail) {
-//            if (element.next == tail) {
-//                tail = element;
-//                tail.next = null; break;
-//            }
-//            element = element.next;
-//        }
-//    }
-//
-//    //метод определения на содержание заданного значения
-//    public void keySearch (String key) {
-//        Linker element = head;
-//        while (element != null) {
-//            if (element.data == key) {
-//                System.out.println("There is the searching element " + key + " at the list. ");
-//                System.out.print('\n');
-//            }
-//            element = element.next;
-//        }
-//    }
-//
-//    //метод удаления из списка заданного значения
-//    public void keySearchAndRemove (String key) {
-//        Linker element = head;
-//        Linker previousElement = head;
-//        while (element.data != null && element.data != key) {
-//            previousElement = element;
-//            element = element.next;
-//        }
-//
-//        if (isEmpty()) {
-//            head = head.next;
-//        } else {
-//            previousElement.next = element.next;
-//        }
-//    }
-//
-//    //метод добавления к элементам списка заданного значения
-//    public void modifyElement (String mod) {
-//        Linker element = head;
-//        if (element != null) {
-//            while (element != null) {
-//                element.data = element.data + mod;
-//                element = element.next;
-//            }
-//            System.out.print('\n');
-//        } else {
-//            throw new IllegalArgumentException("The list is null. ");
-//        }
-//    }
+    //метод извлечения элемента списка (массива) из начала списка
+    public void extractionFromBegin () {
+        Linker element = head;
+        for (int i = 0; i < element.array.length - 1; i++) {
+            System.out.print(element.array[i] + ", ");
+        }
+        System.out.println(element.array[element.array.length-1]);
+    }
+    //метод извлечения элемента списка (массива) из конца списка
+    public void extractionFromEnd () {
+        Linker element = tail;
+        for (int i = 0; i < element.array.length - 1; i++) {
+            System.out.print(element.array[i] + ", ");
+        }
+        System.out.println(element.array[element.array.length-1]);
+    }
 
+    //метод удаления элемента из начала списка
+    public void removingFromBegin () {
+        head = head.next;
+    }
+    //метод удаления элемента списка (массива) из конца списка
+    public void removingFromEnd () {
+        Linker element = head;
+        while (element != tail) {
+            if (element.next == tail) {
+                tail = element;
+                tail.next = null; break;
+            }
+            element = element.next;
+        }
+    }
+
+    //метод определения на содержание заданного значения
+    public void keySearch (String key) {
+        Linker element = head;
+        while (element != null) {
+            for (String i : element.array) {
+                if (i == key) {
+                    System.out.println("There is the searching element " + key + " at the list. ");
+                    System.out.print('\n');
+                }
+            }
+            element = element.next;
+        }
+    }
+
+    //метод удаления из массива элемента списка заданного значения
+    public void keySearchAndRemove (String key) {
+        Linker element = head;
+        Linker elementPrevious;
+        while (element != null) {
+            for (int i = 0; i < element.array.length; i++) {
+                if (element.array[i] == key) {
+                    while (element.array[i] != null) {
+                        if (i == (element.array.length-1)) {
+                            elementPrevious = element;
+                            element = element.next;
+                            elementPrevious.array[elementPrevious.array.length-1] = element.array[0];
+                            i = 0;
+                        } else {
+                            element.array[i] = element.array[i + 1];
+                            i++;
+                        }
+                    }
+                }
+            }
+            element = element.next;
+        }
+    }
+
+    //метод добавления к элементам массива списка заданного значения
+    public void modifyElement (String mod) {
+        Linker element = head;
+        while (element != null) {
+            for (int i = 0; i < element.array.length; i++) {
+                element.array[i] = element.array[i] + mod;
+            }
+            element = element.next;
+        }
+    }
+
+
+
+//        Linker elementNew = new Linker(new String[5]);
+//        while (element != null) {
+//            for (int i = 0; i < element.array.length; i++) {
+//                if (i==0) {
+//
+//                    elementOld.array[i] = element.array[i];
+//                    element.array[i] = elementOld.array[i] + mod;
+//                }
+//                elementOld.array[i] = element.array[i];
+//                element.array[i] = elementOld.array[i] + mod;
+//            }
+//            element = element.next;
+//        }
+//    }
 
     //Getters and Setters
+//            if (i==0 && head == null) {
+//        Linker element = new Linker(new String[5]);
+//        element.array[i] = data;
+//        head = element;
+//        tail = element;
+//        i++;
+//    } else if (i==0) {
+//        Linker element = new Linker(new String[5]);
+//        tail.next = element;
+//        tail = element;
+//        element.array[i] = data;
+//        i++;
+//    } else {
+//        Linker element = tail;
+//        element.array[i] = data;
+//        if (element.array[element.array.length - 1] == null) {
+//            i++;
+//        } else {
+//            i = 0;
+//        }
+//    }
 
 }
--- src/ru/avalon/vergentev/j110/labwork3_3/Main.java
+++ src/ru/avalon/vergentev/j110/labwork3_3/Main.java
@@ -2,70 +2,32 @@ package ru.avalon.vergentev.j110.labwork3_3;
 
 public class Main {
     public static void main(String[] args) {
-        //работа со списком, который формируетс€ с добавлением элементов в начало
         List myList1 = new List();
+        myList1.addToEnd("GGG");
+        myList1.addToEnd("HHH");
+        myList1.addToEnd("III");
+        myList1.addToEnd("JJJ");
+        myList1.addToEnd("KKK");
+        myList1.addToEnd("LLL");
+        myList1.addToEnd("MMM");
         myList1.addToBegin("AAA");
-        myList1.addToBegin("BBB");
-        myList1.addToBegin("CCC");
-        myList1.addToBegin("DDD");
-        myList1.addToBegin("EEE");
-        myList1.addToBegin("FFF");
-        myList1.addToBegin("GGG");
-        myList1.addToBegin("HHH");
-        myList1.addToBegin("III");
-        myList1.addToBegin("JJJ");
-        myList1.addToBegin("KKK");
-        myList1.addToBegin("LLL");
+//        myList1.addToBegin("BBB");
+//        myList1.addToBegin("CCC");
+//        myList1.addToBegin("DDD");
+//        myList1.addToBegin("EEE");
+//        myList1.addToBegin("FFF");
         myList1.print(); //выводим сформированный список
+//        myList1.modifyElement("zzz"); //ƒобавл€ем к элементам списка GGG
+//        myList1.print(); //выводим модифицированный список
 //        myList1.extractionFromBegin(); //извлечение из начала списка без удалени€
 //        System.out.print('\n');
-//        myList1.print(); // проверка того что удалени€ не произошло
-//        myList1.removingFromBegin(); //удаление элемента в начале списка
-//        myList1.print(); // выводим оставшийс€ список на экран
-
-        //работа со списком, который формируетс€ с добавлением элементов в конец
-        List myList2 = new List();
-        myList2.addToEnd("aaa");
-        myList2.addToEnd("bbb");
-        myList2.addToEnd("ccc");
-        myList2.addToEnd("ddd");
-        myList2.addToEnd("eee");
-        myList2.addToEnd("fff");
-        myList2.addToEnd("ggg");
-        myList2.addToEnd("hhh");
-        myList2.addToEnd("iii");
-        myList2.addToEnd("jjj");
-        myList2.addToEnd("kkk");
-        myList2.addToEnd("lll");
-        myList2.print(); //выводим сформированный список
-//        myList2.extractionFromEnd(); //извлечение из конца списка без удалени€
+//        myList1.extractionFromEnd(); //удаление элемента в начале списка
 //        System.out.print('\n');
-//        myList2.print(); // проверка того что удалени€ не произошло
-//        myList2.removingFromEnd(); //удаление элемента в конце списка
-//        myList2.print(); // выводим оставшийс€ список на экран
-
-//        //работа со списком, в котором работает с конкретным элементом
-//        List myList3 = new List();
-//        myList3.addToEnd("aaAAaa");
-//        myList3.addToEnd("bbBBbb");
-//        myList3.addToEnd("ccCCcc");
-//        myList3.addToEnd("ddDDdd");
-//        myList3.print(); //выводим заданный список
-//        myList3.keySearch("ccCCcc"); //провер€ем есть ли в списке элемент
-//        myList3.keySearch("bBb"); //провер€ем есть ли в списке элемент
-//        myList3.keySearchAndRemove("ccCCcc"); //удал€ем заданный элемент
-//        myList3.print(); //выводим заданный список с удалЄнным элементом
-//
-//        //работа со списком, в котором работает с конкретным элементом
-//        List myList4 = new List();
-//        myList4.addToEnd("aaAAaa");
-//        myList4.addToEnd("bbBBbb");
-//        myList4.addToEnd("ccCCcc");
-//        myList4.addToEnd("ddDDdd");
-//        myList4.print(); //выводим заданный список
-//        myList4.modifyElement("GGG"); //ƒобавл€ем к элементам списка GGG
-//        myList4.print(); //выводим изменЄнный список
-
+//        myList1.print(); // проверка того что удалени€ не произошло
+//        myList1.keySearch("DDDzzz"); //провер€ем есть ли в списке элемент
+//        myList1.keySearch("bBbzzz"); //провер€ем есть ли в списке элемент
+//        myList1.keySearchAndRemove("DDDzzz"); //удал€ем заданный элемент
+//        myList1.print(); //выводим заданный список с удалЄнным элементом
 
     }
 }
